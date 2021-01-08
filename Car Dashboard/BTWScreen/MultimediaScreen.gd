13753d0a5ea5da68ee00bae8ec3012671b824123
przadkap <piotrprzadka01@gmail.com>
2021-01-08 15:52:27 +0100

    add song switching

--- Car Dashboard/BTWScreen/MultimediaScreen.gd
+++ Car Dashboard/BTWScreen/MultimediaScreen.gd
@@ -9,16 +9,12 @@ var song_info = [
 ]
 
 var songs = []
+var current_song_index = 0
 
 func _ready():
 	for song in song_info:
 		songs.append({"title": song["title"], "stream": load(song["path"])})
-	$MusicAudio.stream = songs[0]["stream"]
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$MusicAudio.stream = songs[current_song_index]["stream"]
 
 
 func _on_MusicPlayer_music_toggle():
@@ -31,3 +27,15 @@ func _on_MusicPlayer_music_toggle():
 		$MusicAudio.playing = true
 		$MusicAudio.stream_paused = false
 
+
+
+func _on_MusicPlayer_switch_song(index_offset):
+	current_song_index += index_offset
+
+	if current_song_index >= len(songs):
+		current_song_index = 0
+	elif current_song_index < 0:
+		current_song_index = len(songs) - 1
+		
+	$MusicAudio.stream = songs[current_song_index]["stream"]
+	$MusicAudio.play() # only actually plays if the song was playing before for some reason
--- Car Dashboard/MMScreen/MusicPlayer.gd
+++ Car Dashboard/MMScreen/MusicPlayer.gd
@@ -1,6 +1,7 @@
 extends PanelContainer
 
 signal music_toggle
+signal switch_song
 
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +10,10 @@ func _ready():
 func _on_PlayPause_pressed():
 	emit_signal("music_toggle")
 
+
+func _on_PreviousSong_pressed():
+	emit_signal("switch_song", -1)
+
+
+func _on_NextSong_pressed():
+	emit_signal("switch_song", 1)
