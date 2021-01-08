5012ba4bb6706ba3f3f09362199b4bde018b8b4e
przadkap <piotrprzadka01@gmail.com>
2021-01-08 10:21:19 +0100

    add fuel-meter logic

--- Car Dashboard/BTWScreen/FuelMeter/FuelMeter.gd
+++ Car Dashboard/BTWScreen/FuelMeter/FuelMeter.gd
@@ -4,11 +4,12 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var meter_levels = [1, 4, 7, 10, 13]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	fuel_changed(5)
+	load_segment_styleboxes()
+	fuel_changed(95)
 	pass # Replace with function body.
 
 
@@ -25,3 +26,12 @@ func fuel_changed(new_fuel):
 		else:
 			 break # not sure if good idea
 	pass
+
+
+
+func load_segment_styleboxes():
+	for segment in get_children():
+		var level_no = meter_levels[segment.get_index()]
+		var path = "res://styleboxes/level-%s.tres"
+		var level_stylebox = load(path % str(level_no))
+		segment.add_stylebox_override("fg", level_stylebox)
