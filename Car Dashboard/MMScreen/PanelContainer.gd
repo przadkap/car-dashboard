624f7f8ba91bc66dc2074f657de8a32978b3ba05
przadkap <piotrprzadka01@gmail.com>
2021-01-20 13:15:39 +0100

    connect tilebuttons to content in MM

--- Car Dashboard/MMScreen/PanelContainer.gd
+++ Car Dashboard/MMScreen/PanelContainer.gd
@@ -9,8 +9,16 @@ var selected_stylebox = preload("res://MMScreen/styleboxes/multimedia_sidebar.tr
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_stylebox_override("panel", selected_stylebox)
+	add_stylebox_override("panel", tile_stylebox)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_MultimediaScreen_content_changed(_content):
+	add_stylebox_override("panel", selected_stylebox)
+	if($GridContainer.visible):
+		$GridContainer.hide()
+	if(!$SideSeparator.visible):
+		$SideSeparator.show()
